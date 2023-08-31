99db342a4f299ba7d3f39b59be62854d32703743
Andrey Vlasov <avlasov@cryptomatch.app>
2023-08-31 12:08:50 +0400

    Fix configs

--- nix/hix.nix
+++ nix/hix.nix
@@ -13,6 +13,5 @@
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
   shell.tools.haskell-language-server = "latest";
-  shell.tools.hlint = "latest";
-  shell.tools.stack = "2.11.1";
+  # shell.tools.hlint = "latest";
 }
